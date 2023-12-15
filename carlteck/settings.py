3dc7c1a0c16c4ed0d1ea3aa5c92bdb3b5212db81
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-12-15 16:35:18 +0500

    code updated

--- carlteck/settings.py
+++ carlteck/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7wv$*p($bv(stlel1#h--#u-@!nzk6%38(v92ufgh-%ujaw7o@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["carlteck.com", "127.0.0.1"]
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'carlteck.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR /'assets']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
