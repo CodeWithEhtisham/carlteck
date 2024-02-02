ec29f55a0639b16347ce65c5206668da49c98712
HamxAnwar <hamzaanwar93@outlook.com>
2024-02-02 13:22:44 +0500

    Completed jenkins file update.

--- conf/virtual_env.sh
+++ conf/virtual_env.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# cd to ~
+cd ~
+
 # Install necessary packages
 sudo apt update && sudo apt install python3-pip nginx curl
 
@@ -12,6 +15,7 @@ then
 else
     cd carlteck
     echo "Directory exists: $PWD"
+    git pull
 fi
 
 # Checking if virtual environment exists
@@ -39,13 +43,51 @@ fi
 # Activating the virtual environment
 source env/bin/activate
 
-# Pull the latest code from the repository
-git pull
-
 # Install dependencies
-pip3 install django gunicorn
+pip3 install -r ./requirements.txt
 
-./manage.py makemigrations && ./manage.py migrate
+./manage.py makemigrations
+./manage.py migrate
 
 sudo ufw allow 8000
 
+# Come out of the virtual environment
+deactivate
+
+# Check for the socket file
+if [ ! "/etc/systemd/system/gunicorn.socket" ]
+then
+    cp ./conf/gunicorn.socket /etc/systemd/system/gunicorn.socket
+else
+    sudo rm /etc/systemd/system/gunicorn.socket
+    cp ./conf/gunicorn.socket /etc/systemd/system/gunicorn.socket
+fi
+
+# Check for the service file
+if [ ! "/etc/systemd/system/gunicorn.service" ]
+then
+    cp ./conf/gunicorn.service /etc/systemd/system/gunicorn.service
+else
+    sudo rm /etc/systemd/system/gunicorn.service
+    cp ./conf/gunicorn.service /etc/systemd/system/gunicorn.service
+fi
+
+# Start / Enable the service
+sudo systemctl enable --now gunicorn.socket
+
+# Check for sites-available
+if [ ! -d "/etc/nginx/sites-available" ]
+then
+    echo "Directory does not exist. Creating one."
+    mkdir -p /etc/nginx/sites-available
+    echo "Directory created"
+    cp ./conf/carlteck /etc/nginx/sites-available/carlteck
+else
+    rm /etc/nginx/sites-available/carlteck
+    cp ./conf/carlteck /etc/nginx/sites-available/carlteck
+fi
+
+# Make the link to activate nginx configuration
+sudo ln -s /etc/nginx/sites-available/carlteck /etc/nginx/sites-enabled/
+
+sudo systemctl enable --now nginx
