06ec400f9b16637819f5165f9962d3918810a453
CodeWithEhtisham <shamikhan0347@gmail.com>
2024-02-02 17:10:25 +0500

    update site enable

--- carlteck/settings.py
+++ carlteck/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7wv$*p($bv(stlel1#h--#u-@!nzk6%38(v92ufgh-%ujaw7o@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["carlteck.com", "127.0.0.1","www.carlteck.com"]
+ALLOWED_HOSTS = ["carlteck.com","www.carlteck.com","127.0.0.1"]
 
 
 # Application definition
