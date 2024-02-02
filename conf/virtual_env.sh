7a87241cefd55aa39d47c08680cdc25ec9c817b3
HamxAnwar <hamzaanwar93@outlook.com>
2024-02-02 16:41:53 +0500

    updating gunicorn user

--- conf/virtual_env.sh
+++ conf/virtual_env.sh
@@ -5,16 +5,6 @@ if [ ! -d "env" ]
 then
     echo "Virtual environment does not exist. Creating one."
 
-    if virtualenv --version &> /dev/null
-    then
-        echo "virtualenv is installed."
-    else
-        echo "virtualenv is not installed. Installing..."
-        sudo apt install python3-virtualenv
-        echo "Virtualenv Installed"
-    fi
-
-
     # Creating a virtual environment named 'env'
     python3.8 -m venv env
     echo "Virtual environment created."
